7015c52feac84b73ca29981aafd06c7eb5d74761
Praneethsai24 <spraneeth698@gmail.com>
2026-03-02 00:49:45 +0530

    Age prediction without API key

---
+++ AI/age_prediction.py
@@ -0,0 +1,11 @@
+import requests
+name = "harshini"
+
+url = f"https://api.agify.io/?name={name}"
+
+response = requests.get(url)
+data = response.json()
+
+print("Name:", data["name"])
+print("Predicted Age:", data["age"])
+print("Confidence:", data["count"])
