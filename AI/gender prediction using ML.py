ca8d2ff77f0f2f194d677bada693d3c9c3f30560
Praneethsai24 <spraneeth698@gmail.com>
2026-03-02 22:17:36 +0530

    Half finished loop code

--- AI/gender prediction using ML.py
+++ AI/gender prediction using ML.py
@@ -1,3 +1,4 @@
+'''
 import requests
 
 name = input("Name: ")
@@ -8,5 +9,22 @@ response = requests.get(url)
 data = response.json()
 
 print(f"Seems like {data['name']} is {data['gender']} and data points used are {data['count']}")
-
+'''
 # Here gneder is a Model's Prediction
+
+
+# We use loop Now!
+
+import requests
+
+results = []
+
+for i in range(5):
+    name = input(f"Name {i+1}: ")
+
+    url = f"https://api.genderize.io/?name={name}"
+    response = requests.get(url)
+    data = response.json()
+
+    results.append(data)
+    
