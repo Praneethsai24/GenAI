099ec84271e664c06021be355de94d2576484ae3
Praneethsai24 <spraneeth698@gmail.com>
2026-03-16 13:36:24 +0530

    Last commit

--- AI/supervised.py
+++ AI/supervised.py
@@ -1,4 +1,33 @@
-import requests
+import requests 
+import json
 
-text = "This product exceeds all my expectations"
 
+examples = [
+    {"text" : "Limited offer", "label" : "spam"},
+    {"text" : "You Got a reward, click here", "label" : "spam"},
+    {"text" : "Meeting scheduled", "label" : "not spam"},
+    {"text" : "Your OPT is", "label" : "not spam"}
+]
+
+new_mail = "Congratulations on winning a reward"
+
+prompt = "Classify this email as spam or not spam.\n\n"
+
+for ex in examples :
+    prompt += f"Email: {ex['text']}\nLabel: {ex['label']}\n\n"
+
+prompt += f"Email: {new_mail}\nLabel:"
+
+response = requests.post(
+    "http://localhost:11434/api/generate",
+    json={
+        "model": "phi3.5:3.8b",
+        "prompt": prompt,
+        "stream": False
+    }
+)
+
+result = response.json()
+
+print("Model prediction")
+print(result["response"])
